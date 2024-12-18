45c07a250cf2fb3f5fc3af000ea266a317aee521
BoBo262 <79087084+BoBo262@users.noreply.github.com>
2024-12-18 17:44:54 +0200

    milestone 3

--- Project.py
+++ Project.py
@@ -5,44 +5,44 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 # Imagini statice:
-IMAGE_FILES = ["Hands1.jpg", "Hands2.jpg", "Hands3.jpg", "NotHand1.jpg", "NotHand2.jpg"]
-OUTPUT_PATH = "output_/"
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=9,
-    min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
+# IMAGE_FILES = ["Hands1.jpg", "Hands2.jpg", "Hands3.jpg", "NotHand1.jpg", "NotHand2.jpg"]
+# OUTPUT_PATH = "output_/"
+# with mp_hands.Hands(
+#     static_image_mode=True,
+#     max_num_hands=9,
+#     min_detection_confidence=0.5) as hands:
+#   for idx, file in enumerate(IMAGE_FILES):
     
-    image = cv2.flip(cv2.imread(file), 1)
+#     image = cv2.flip(cv2.imread(file), 1)
   
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+#     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
-      mp_drawing.draw_landmarks(
-          annotated_image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS,
-          mp_drawing_styles.get_default_hand_landmarks_style(),
-          mp_drawing_styles.get_default_hand_connections_style())
-      output_file = f"{OUTPUT_PATH}{idx}_annotated.jpg"
-      cv2.imwrite(output_file, annotated_image)
+#     print('Handedness:', results.multi_handedness)
+#     if not results.multi_hand_landmarks:
+#       continue
+#     image_height, image_width, _ = image.shape
+#     annotated_image = image.copy()
+#     for hand_landmarks in results.multi_hand_landmarks:
+#       print('hand_landmarks:', hand_landmarks)
+#       print(
+#           f'Index finger tip coordinates: (',
+#           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+#           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+#       )
+#       mp_drawing.draw_landmarks(
+#           annotated_image,
+#           hand_landmarks,
+#           mp_hands.HAND_CONNECTIONS,
+#           mp_drawing_styles.get_default_hand_landmarks_style(),
+#           mp_drawing_styles.get_default_hand_connections_style())
+#       output_file = f"{OUTPUT_PATH}{idx}_annotated.jpg"
+#       cv2.imwrite(output_file, annotated_image)
 
-    if not results.multi_hand_world_landmarks:
-      continue
-    for hand_world_landmarks in results.multi_hand_world_landmarks:
-      mp_drawing.plot_landmarks(
-        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+#     if not results.multi_hand_world_landmarks:
+#       continue
+#     for hand_world_landmarks in results.multi_hand_world_landmarks:
+#       mp_drawing.plot_landmarks(
+#         hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
 
 # Realtime detection:
 cap = cv2.VideoCapture(0)
@@ -64,6 +64,11 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        for id, lm in enumerate(hand_landmarks.landmark):
+          h, w, c = image.shape
+          cx, cy = int(lm.x*w), int(lm.y*h)
+          print(cx,cy)
+
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
